8d449b6130b2108c1e571b63a4e9fab925484c55
Sachin Varghese <sachinv2223@gmail.com>
2022-08-06 21:16:46 +0530

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weaterApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderVale: '88f266f83bmshee519f2555fed09p1f23e6jsn9c0e475b6637'
 };
